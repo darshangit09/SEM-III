76225784d448aa149f07ac9a87172aef3574fd6a
Mr.Finaviya <100658373+darshangit09@users.noreply.github.com>
2022-09-13 23:27:10 +0530

    Update array.cpp

--- Data Structure/array.cpp
+++ Data Structure/array.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-    int main()
-{   
+class student
+{
     int num[10];
     string name[10], city[10];
+    public:
+    void getdata();
+    void putdata();
+};
+   void student::getdata()
+   {
     cout<<"Enter enrollement_no, Name and City for 5 students:";
     for(int i=1;i<=5;i++)
     {    
@@ -15,5 +21,23 @@ using namespace std;
             cin>>city[i];
         
     }
+   }
+   void student::putdata()
+   {
+    for(int i=1;i<=5;i++)
+    {    
+        cout<<"Detail of Student"<<i<<":";
+        cout<<num[i];
+        cout<<name[i];
+        cout<<city[i];
+        cout<<endl;
+    }   
+   }
+   
+    int main()
+{   
+    student s;
+    s.getdata();
+    s.putdata();
     return 0;
 }
