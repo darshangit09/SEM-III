1a2aa59544af08aef15a8b8332628a14384d4bf1
Mr.Finaviya <100658373+darshangit09@users.noreply.github.com>
2022-09-19 12:40:42 +0530

    Update employee.java

--- OOPJ/employee.java
+++ OOPJ/employee.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 class employee1
 {
     
-    int[] emp_num= new int[5];
-    String[] name = new String[5];
-    String[] deg=new String[5];
+    int[] emp_num= new int[6];
+    String[] name = new String[6];
+    String[] deg=new String[6];
     void get()
     {
         Scanner s = new Scanner(System.in);      
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<6;i++)
         {
-        System.out.println("Enter Name of Employee");
-        name[i]=s.nextLine();
+        System.out.println("Enter Name of Employee:"+i);
+        name[i]=s.next();
         System.out.println("Enter Employee No");
         emp_num[i]=s.nextInt();
-        Scanner s1 = new Scanner(System.in); 
         System.out.println("Enter Degination of Employee");
-        deg[i]=s1.nextLine();
+        deg[i]=s.next();
         }
     }
 }
-class salary extends employee1
+class darsh extends employee1
 {     
   
    
@@ -28,28 +27,28 @@ class salary extends employee1
 {    
       Scanner s = new Scanner(System.in); 
    
-      int bp[]=new int[5];
-      int hra[]=new int[5];  
-      System.out.println("Enter Basic pay of Employee");
-      System.out.println("Enter Home Rent Allowance");
-       for(int i=1;i<=5;i++)
+      int bp[]=new int[6];
+      int hra[]=new int[6];  
+       for(int i=1;i<6;i++)
         {
-        System.out.println("Enter details of employee:"+i);
+        System.out.println("Enter Basic pay of Employee:"+i);
         bp[i]=s.nextInt();
+        System.out.println("Enter Home Rent Allowance");
         hra[i]=s.nextInt();
         }
-          for(int i=1;i<=5;i++)
+          for(int i=1;i<6;i++)
         {
          int salary=bp[i]+hra[i]*bp[i]+hra[i];
          System.out.println("Salary of Employee:"+i+"is"+salary);
         }
 }
-} 
+}
+
 class employee
 {
  public static void main(String args[])
 {
-  salary q=new salary();
+  darsh q=new darsh();
   q.get();
   q.salary();
 }
