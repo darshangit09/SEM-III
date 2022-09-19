eb70fa883ad14abf1705402f7ca819af1d98eee9
Mr.Finaviya <100658373+darshangit09@users.noreply.github.com>
2022-09-19 11:13:14 +0530

    Update bank.java

--- OOPJ/bank.java
+++ OOPJ/bank.java
@@ -1,58 +1,81 @@
 import java.util.Scanner;
-class Bank
+class bank1
 {
-    int acc_num;
-    String name;
-    double bal;
+    
+    int[] acc_num= new int[5];
+    int v;
+    String[] name = new String[5];
+    double[] bal=new double[5];
     Scanner s = new Scanner(System.in);
     void get()
     {
         System.out.println("Enter Name");
-        name = s.next();
+        for(int i=0;i<5;i++)
+        {
+        name[i]=s.nextLine();
+                
+        }
 
         System.out.println("Enter Account No");
-        acc_num = s.nextInt();
+        for(int i=0;i<5;i++)
+        {
+        acc_num[i]=s.nextInt();
+                
+        }
   
         System.out.println("Enter Balance");
-        bal = s.nextDouble();
+        for(int i=0;i<5;i++)
+        {
+        bal[i]=s.nextDouble();
+                
+        }
     }
    
-   void deposite()
-    {
-        int amt;
-        System.out.println("Enter amount you want to deposite");
-        amt = s.nextInt();
-        
-        bal = bal + amt;               
-    }
-
-    void withdraw()
-    {
-        int amt;
-        System.out.println("Enter amount you want to withdraw");
-        amt = s.nextInt();
-        
-        bal = bal - amt;
-    }
-
     void display()
-    {
-        System.out.println("Name of the Account Holdder is: "+ name);
-        System.out.println("The Account Number is: "+ acc_num);
-        System.out.println("Balance of the Account is: "+ bal);
+    {    for(int i=0;i<5;i++)
+        {
+        System.out.println("Name of the Account Holdder is: "+name[i]);
+        }       
+        
+         
+        for(int i=0;i<5;i++)
+        {        
+        System.out.println("The Account Number is: "+acc_num[i]);
+        }
+        
+         for(int i=0;i<5;i++)
+        {        
+        System.out.println("Balance of the Account is: "+bal[i]);
+        }
     }
+        void search()
+        {
+                            
+          System.out.println("Entert the account number to search the deatails of account holder: ");
+          v=s.nextInt();
+                for(int i=0;i<5;i++)
+                {        
+                        if(acc_num[i]==v)
+                        {
+                               System.out.println("Name of holder is ::"+name[i]);
+                               System.out.println("Accunt number of holder is ::"+acc_num[i]);
+                               System.out.println("Balance of holder is ::"+bal[i]);
+                        }        
+                
+        
+                }
+
+        }
 }
 
 class bank
 {
-    public static void main(String a[])
+    public static void main(String args[])
     {
-        Bank b = new Bank();
-
+        bank1 b = new bank1();
         b.get();
-        b.deposite();
-        b.display();
-        b.withdraw();
         b.display();   
+        b.search();
+        
     }
 }
